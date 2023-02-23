f805c4d87d6ce044b0d49f4d4b07b2a0a838889b
Piyush Varman (PCV) <varmanpiyush@gmail.com>
2023-02-23 17:59:21 +0530

    Update Random Number Guessing Game Tkinter.py

--- Class XI- Python Programs/Fun and Games/Random Number Guessing Game Tkinter.py
+++ Class XI- Python Programs/Fun and Games/Random Number Guessing Game Tkinter.py
@@ -25,13 +25,15 @@ def guess():
             result.place(x=250,y=310,anchor="center")
     else:
         messagebox.showerror("Hold on!","The input should be\na number from 1 to 10!")
+    
+    g.delete(0,END)
 Rule=Label(root,text="Enter a number from 1 to 10.\nSee if you got it right!",font=("Consolas",20),bg="#FFD700")
 Rule.place(x=250,y=50,anchor="center")
 result=Label(root,text="",font=("Calbri",25,"italic"),fg='green',bg="#FFD700")
 result.place(x=250,y=300,anchor="center")
 choice=Label(root,text="",font=("Calibri",50),bg="#FFD700")
 choice.place(x=250,y=150,anchor="center")
-g=Entry(root,font=("Georgia",15),justify="center")
+g=Entry(root,font=("Georgia",15))
 g.place(x=250,y=400,anchor="center")
 b=Button(root,text="Guess!",font=("Consolas",12,"bold"),command=guess,bg="#39FF14",activebackground='red',borderwidth=2)
 b.place(x=250,y=460,anchor="center")
