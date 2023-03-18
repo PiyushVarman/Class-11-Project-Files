1a9f2978a13e520ac124f51fed9220ebd340ef87
Piyush Varman (PCV) <varmanpiyush@gmail.com>
2023-03-18 09:06:59 +0530

    Update Q33-1-2.py

--- Class XI- Python Programs/Examination Programs/Half Yearly/Q33-1-2.py
+++ Class XI- Python Programs/Examination Programs/Half Yearly/Q33-1-2.py
@@ -2,7 +2,7 @@ n=input("Enter the name of the employee:")
 sal=int(input("Enter "+n+"'s salary:"))
 if sal>20000:
     bon=(20/100)*sal
-elif sal>10000:
+elif sal>=10000:
     bon=(18/100)*sal
 else:
     bon=(16/100)*sal
